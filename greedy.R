681c92e8b756c26f4d0af07a7e8f216c240db50e
Cristopher <cristopherpk@gmail.com>
2016-08-10 18:23:43 -0300

    few changes

--- greedy.R
+++ greedy.R
@@ -1,10 +1,24 @@
-greedy <- function(T = matrix(), k, thresh){
-  ## initializing empty lists
-  s <- list(); # results
-  t <- list(); # remaining time for a vehicle
+greedy <- function(T, k, thresh){
   
-  while(k > 0){
-    
+  # initial set with all intersections. 
+  S <- 1:nrow(T)
+  # initializing empty lists
+  s <- c()
+  
+  t <- colSums(T)
+  W <- rowSums(T)
+  
+  tj <- list()
+  which(t == min(t))
+  # which(rowSums(T) == max(rowSums(T))
+  # colSums(T), sum of time vehicle spends in all intersections.
+  while(k > 0 || length(S) == 0){
+    for(j in 1:ncol(T)){
+      tj <- append(t, as.integer(0));
+      for(i in 1:nrow(T)){
+        t[[j]] <- T[j][[1]][i] + t[[j]];
+
+      }
+    }
   }
-  
 }
