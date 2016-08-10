6ff6097dc0f014438302180de02f5a79e1d9f0c1
Cristopher <cristopherpk@gmail.com>
2016-08-10 08:52:23 -0300

    first commit

---
+++ greedy.R
@@ -0,0 +1,10 @@
+greedy <- function(T = matrix(), k, thresh){
+  ## initializing empty lists
+  s <- list(); # results
+  t <- list(); # remaining time for a vehicle
+  
+  while(k > 0){
+    
+  }
+  
+}
