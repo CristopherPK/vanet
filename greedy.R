a66ecc97526443ec2e597faee67652a7139083ce
Cristopher <cristopherpk@gmail.com>
2016-08-11 16:40:59 -0300

    greedy approach working.

--- greedy.R
+++ greedy.R
@@ -1,24 +1,43 @@
 greedy <- function(T, k, thresh){
   
   # initial set with all intersections. 
-  S <- 1:nrow(T)
-  # initializing empty lists
-  s <- c()
+  S <- 1:nrow(T);
   
-  t <- colSums(T)
-  W <- rowSums(T)
+  # initializing empty result list.
+  s <- c();
   
-  tj <- list()
-  which(t == min(t))
+  #t <- colSums(T)
+  #W <- rowSums(T)
+  
   # which(rowSums(T) == max(rowSums(T))
   # colSums(T), sum of time vehicle spends in all intersections.
   while(k > 0 || length(S) == 0){
+    tj <- list()
+    W <- c(rep.int(x = 0,times = nrow(T)))
+    
+    # for each vehicle.
     for(j in 1:ncol(T)){
-      tj <- append(t, as.integer(0));
+      tj <- append(tj, as.integer(0));
+      # for each intersection. 
       for(i in 1:nrow(T)){
-        t[[j]] <- T[j][[1]][i] + t[[j]];
-
+        if(!(i %in% s)){ # check if intersection wasn't chosen already.
+          T_ij <- T[j][[1]][i];
+          if(T_ij > (thresh - tj[[j]])){
+            W[[i]] <- W[[i]] + (thresh - tj[[j]]);
+            tj[[j]] <- 0
+          } else {
+            tj[[j]] <- tj[[j]] + T_ij;
+            W[[i]] <- W[[i]] + T_ij;
+          }
+        }
       }
     }
+    
+    w <- which(W == max(W));
+    s <- (append(s, w));
+    k = k-1;
   }
+  
+  s;
+  
 }
