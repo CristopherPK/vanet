7c975af1b41d712953437fe3cbf523f238b55cbe
Cristopher <cristopherpk@gmail.com>
2016-08-14 01:12:58 -0300

    developing genetic approach.

--- genetic.R
+++ genetic.R
@@ -1,36 +1,149 @@
-genetic <- function(T, k, thresh){
+GeneticAlgorithm <- function(T, k, thresh){
+  # Calculating boundaries
+  num.Vehicles <- nrow(T)
+  num.Inter <- length(T[[1]])
+  
   # initial set with all intersections. 
-  S <- 1:nrow(T);
+  S <- num.Inter
   
   # initializing empty result list.
-  s <- c();
+  s <- c()
   
   # population size
-  pop_size <- 400;
-  population <- data.frame();
+  num.Gen <- 100
+  p.Cross <- 0.95
+  p.Mut <- 0.1
+  pop.Size <- 400
   
+  
+  print("initializing population.")
+  
+  population <- data.frame()
   # generating random population.
-  pop_rand <- ceiling(pop_size/2); # half of the population is going to be random.
-  for(i in 1:pop_rand){
-    population <- rbind(population, sample(1:nrow(T), k));
+  pop.Rand <- ceiling(pop.Size/2) # half of the population is going to be random.
+  for(i in 1:pop.Rand){
+    population <- rbind(population, sample(1:num.Inter, k))
   }
+  print("random population generated.")
   
   # generating greedy population.
-  pop_greedy <- pop_size - pop_rand;
-  for(i in 1:pop_greedy){
-    population <- rbind(population, greedy(T, k, thresh));
+  pop.Greedy <- pop.Size - pop.Rand
+  for(i in 1:pop.Greedy){
+    population <- rbind(population, Greedy(T, k, thresh))
+  }
+  print("greedy population generated.")
+  
+  print("starting evolutionary approach.")
+  while(num.Gen > 0){
+    
+    # calculating population fitness.
+    pop.Fitness <- c()
+    for(i in 1:nrow(population)){
+      pop.Fitness <- cbind(pop.Fitness,CalcFitness(T,unlist(population[i,])))
+    }
+    
+    # performing tournament selection with size 2. 
+    # TODO: edit this function to accept different size of tournament selection.
+    parents <- TournamentSelection(pop.Fitness)
+    
+    # Execute one-point crossover with probability pcross
+    childs <- GenerateChilds(population, parents, num.Inter)
+    
+    # Execute one-point mutation with probability pmut
+    childs <- DoMutation(num.Inter, childs)
+    
+    # Choose the elitist population for the next generation
+    
+    # Get the best from the first generation as criteria. 
+    which(pop.Fitness == max(pop.Fitness))
+    
+    num.Gen <- num.Gen - 1
   }
   
-  while(k > 0){
-    
-    
-    k <- k -1;
-  }
-  
-  s;
+  s
   
 }
 
-fitness <- function(genome){
-  
+# Fitness calculation based on vehicles coverage.
+CalcFitness <- function(T, genome){
+  # subsetting those in the genome. 
+  V <- T[genome,]
+  V.Sum <- sapply(V,sum)
+  n.Covered <- length(which(V.Sum >= thresh))
+  coverage <- (n.Covered/nrow(T))*100
+  coverage
+}
+
+# Tournament selection with size 2 function.
+TournamentSelection <- function(pop.Fitness, size = 2){
+  parents <- c()
+  pop.Rand <- sample(1:length(pop.Fitness))
+  for(i in 1:length(pop.Rand)){
+    if((i %% 2) > 0){
+      x1 <- pop.Fitness[pop.Rand[i]]
+      x2 <- pop.Fitness[pop.Rand[i+1]]
+      if(x1 > x2){
+        parents <- cbind(parents,pop.Rand[i])
+      } else {
+        parents <- cbind(parents,pop.Rand[i+1])
+      }
+    }
+  }
+  parents
+}
+
+CorrectRepetition <- function(genome,num.Inter){
+  g <- genome
+  while(anyDuplicated(g) > 0){
+    g[anyDuplicated(g)] <- sample(1:num.Inter, size = 1)
+  }
+  g
+}
+
+# Create a new generation based on the crossover method chosen.
+GenerateChilds <- function(population, parents, num.Inter, crossover.Points = 1){
+  childs <- c()
+  len.Genome <- length(population[parents[1],])
+  for(p in 1:length(parents)){
+    if((i %% 2) > 0){
+      # add the pcross as criteria for reproduction.
+      if(sample(0:1, size = 1, prob = c(1-p.Cross, p.Cross))){
+        point <- sample(1:len.Genome,size = 1)
+        
+        p1 <- unlist(population[parents[i],])
+        t1 <- p1[1:point-1]
+        t3 <- p1[point:length(p1)]
+        
+        p2 <- unlist(population[parents[i+1],])
+        t2 <- p2[point:length(p2)]
+        t4 <- p2[1:point-1]
+        
+        c1 <- append(t1,t2)
+        names(c1) <- NULL
+        c1 <- CorrectRepetition(c1,num.Inter)
+        childs <- rbind(childs, c1)
+        
+        c2 <- append(t3,t4)
+        names(c2) <- NULL
+        c2 <- CorrectRepetition(c1,num.Inter)
+        childs <- rbind(childs, c2)
+      }
+    }
+  }
+  childs
+}
+
+# Doing mutation based on its probability
+DoMutation <- function(num.Inter, childs){
+  childs.Mut <- c()
+  for(i in 1:nrow(childs)){
+    genome <- childs[i,]
+    if(sample(0:1, size = 1, prob = c(1-p.Mut, p.Mut))){
+      mut.Gene <- sample(1:length(genome), size = 1)
+      genome[mut.Gene] <- sample(1:num.Inter, size = 1)
+    }
+    genome <- CorrectRepetition(genome, num.Inter)
+    childs.Mut <- rbind(childs.Mut, genome)
+  }
+  childs.Mut
 }
--- greedy.R
+++ greedy.R
@@ -1,29 +1,33 @@
-greedy <- function(T, k, thresh){
+GreedyAlgorithm <- function(T = matrix(), k, thresh){
+  profvis({
+  # Calculating boundaries
+  num.Vehicles <- nrow(T)
+  num.Inter <- length(T[[1]])
   
   # initial set with all intersections. 
-  S <- 1:nrow(T);
+  S <- 1:num.Inter
   
   # initializing empty result list.
-  s <- c();
+  s <- c()
 
   while(k > 0 || length(S) == 0){
-    tj <- list()
-    W <- c(rep.int(x = 0,times = nrow(T)))
+    W <- c(rep.int(x = 0,times = num.Inter))
+    tj <- c(rep.int(x = 0,times = num.Vehicles))
     
     # for each vehicle.
-    for(j in 1:ncol(T)){
-      tj <- append(tj, as.integer(0));
+    for(j in 1:num.Vehicles){
       # for each intersection. 
-      for(i in 1:nrow(T)){
+      Tj <- T[[j]]
+      for(i in 1:num.Inter){
         if(!(i %in% s)){ # check if intersection wasn't chosen already.
-          T_ij <- T[j][[1]][i];
-          if(T_ij > 0){ # avoiding waste of processing time with zero values. 
-            if(T_ij > (thresh - tj[[j]])){
-              W[[i]] <- W[[i]] + (thresh - tj[[j]]);
-              tj[[j]] <- 0
+          Tij <- Tj[i]
+          if(Tij > 0){ # avoiding waste of processing time with zero values. 
+            if(Tij > (thresh - tj[j])){
+              W[i] <- W[i] + (thresh - tj[j])
+              tj[j] <- 0
             } else {
-              tj[[j]] <- tj[[j]] + T_ij;
-              W[[i]] <- W[[i]] + T_ij;
+              tj[j] <- tj[j] + Tij
+              W[i] <- W[i] + Tij
             }
           }
         }
@@ -31,16 +35,15 @@ greedy <- function(T, k, thresh){
     }
     
     # this line was changed to be used as input to the genetic approach.
-    # w <- which(W == max(W)); 
+    # w <- which(W == max(W)) 
     
     # genetic approach change
     Ws <- sort(W,decreasing = TRUE)
     w <- which(W == sample(Ws[1:10],size = 1))
-    s <- (append(s, w));
-    k = k-1;
     
+    s <- (append(s, w))
+    k = k-1
   }
-  
-  s;
-  
+  })
+  s
 }
